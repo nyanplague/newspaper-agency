6d0ed7f95c5bf3c782ab855caf6cb380fc571c0b
Yeva <yevabrookman@gmail.com>
2023-10-03 17:54:57 +0300

    Rewrite script without NLTK

--- agency/templatetags/custom_filters.py
+++ agency/templatetags/custom_filters.py
@@ -1,11 +1,13 @@
 from django import template
 from django.utils.html import strip_tags
-import nltk
+import re
 
 register = template.Library()
 
 
 @register.filter
 def first_two_sentences(content):
-    sentences = nltk.sent_tokenize(strip_tags(content))
-    return " ".join(sentences[:2])
+    content_stripped = strip_tags(content)
+    sentences = re.split(r'(?<=[.!?])\s', content_stripped)
+
+    return ' '.join(sentences[:2])
